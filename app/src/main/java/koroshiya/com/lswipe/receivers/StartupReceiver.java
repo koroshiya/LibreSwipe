f4fa0dae214f3f6c433730f2cda48d6928487609
koroshiya <korocresent@gmail.com>
2016-03-03 14:17:45 +1100

    Implemented "start LibreSwipe on boot" setting

--- app/src/main/java/koroshiya/com/lswipe/receivers/StartupReceiver.java
+++ app/src/main/java/koroshiya/com/lswipe/receivers/StartupReceiver.java
@@ -3,7 +3,9 @@ package koroshiya.com.lswipe.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 
+import koroshiya.com.lswipe.R;
 import koroshiya.com.lswipe.services.SwipeService;
 
 /**
@@ -15,7 +17,12 @@ public class StartupReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            context.startService(SwipeService.getIntent(context));
+            boolean startOnBoot = PreferenceManager
+                                    .getDefaultSharedPreferences(context)
+                                    .getBoolean(context.getString(R.string.pref_startup), false);
+            if (startOnBoot){
+                context.startService(SwipeService.getIntent(context));
+            }
         }
     }
 }
